4e65a80b663a580860accc2b797bc0cb28e40670
JP-Garcia2 <garcia3334444@gmail.com>
2023-10-26 15:04:34 -0500

    todo: make spline from data.csv

--- Assets/BallThrower.cs
+++ Assets/BallThrower.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.IO;
 
 public class BallThrowerController : MonoBehaviour
 {
+
+    public string fileName = "data.csv"; // Name of the CSV file
+    // public GameObject ballPrefab; // The GameObject whose position you want to set
+    private List<Vector3> positions = new List<Vector3>();
+    private int currentIndex = 0;
+
+
     [Header("Configuration")]
     public GameObject ballPrefab;
     public Button throwButton;
@@ -50,63 +59,43 @@ public class BallThrowerController : MonoBehaviour
         if (currentBall != null)
             Destroy(currentBall);
 
-        Vector3 start = new Vector3(startingHeight, startingSide, startingExtension);
-        currentBall = Instantiate(ballPrefab, start, Quaternion.identity);
-        StartCoroutine(MoveBallOnParabola(currentBall));
-    }
-
-    IEnumerator MoveBallOnParabola(GameObject ball)
-    {
-        Vector3 controlPoint = CalculateControlPoint();
-
-        float t = 0;
-        Vector3 start = new Vector3(startingHeight, startingSide, startingExtension);
-        Vector3 end = new Vector3(endingHeight, endingSide, startingExtension);
-        Vector3 previousPosition = start;
-        while (t < 1)
+        string filePath = Path.Combine(Application.dataPath, fileName);
+        if (File.Exists(filePath))
         {
-            t += Time.deltaTime;
-            Vector3 newPos = CalculateBezierPoint(t, start, controlPoint, end);
-            Vector3 velocityDirection = (newPos - previousPosition).normalized;
-            float currentVelocity = Mathf.Lerp(startVelocity, endVelocity, t);
-            ball.transform.position += velocityDirection * currentVelocity * Time.deltaTime;
-            previousPosition = ball.transform.position;
-            ApplySpin(ball.GetComponent<Rigidbody>(), spinAxis, tilt, spinRate);
-            yield return null;
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(',');
+                if (values.Length >= 3)
+                {
+                    float x = float.Parse(values[0]);
+                    float y = float.Parse(values[1]);
+                    float z = float.Parse(values[2]);
+
+                    positions.Add(new Vector3(x, y, z));
+                }
+            }
+
+            InvokeRepeating("SetPositionFromData", 0.2f, 0.2f);
         }
-
-        yield return new WaitForSeconds(2); // Let physics take over for 2 seconds
-        Destroy(ball);
+        else
+        {
+            Debug.LogError("File not found: " + filePath);
+        }
     }
 
-    Vector3 CalculateControlPoint()
+    void SetPositionFromData()
     {
-        Vector3 midPoint = (new Vector3(startingHeight, startingSide, startingExtension) +
-                            new Vector3(endingHeight, endingSide, startingExtension)) / 2;
-        return new Vector3(midPoint.x + horizontalBreak, midPoint.y + inducedVerticalBreak,
-                           midPoint.z + verticalBreak);
-    }
-
-    Vector3 CalculateBezierPoint(float t, Vector3 start, Vector3 control, Vector3 end)
-    {
-        float u = 1 - t;
-        Vector3 point = (u * u * start) + (2 * u * t * control) + (t * t * end);
-        return point;
-    }
-
-    void ApplySpin(Rigidbody rb, float spinAxisDegrees, float tiltDegrees, float spinRateRPM)
-    {
-        // Convert spin axis and tilt into a 3D rotation axis
-        Vector3 spinDirection = new Vector3(Mathf.Sin(spinAxisDegrees * Mathf.Deg2Rad) * Mathf.Sin(tiltDegrees * Mathf.Deg2Rad),
-                                            Mathf.Cos(tiltDegrees * Mathf.Deg2Rad),
-                                            Mathf.Cos(spinAxisDegrees * Mathf.Deg2Rad) * Mathf.Sin(tiltDegrees * Mathf.Deg2Rad));
-        Vector3 spin = spinDirection.normalized * (spinRateRPM * 2f * Mathf.PI / 60f); // Convert RPM to radians per second
-        rb.AddTorque(spin);
+        if (currentIndex < positions.Count)
+        {
+            ballPrefab.transform.position = positions[currentIndex];
+            currentIndex++;
+        }
+        else
+        {
+            Debug.Log("End of data reached.");
+            CancelInvoke("SetPositionFromData");
+        }
     }
 }
-
-
-
-
-
-
