a03215e08fd2b588f6021ec9a9fdf278eb13fc51
JP-Garcia2 <garcia3334444@gmail.com>
2023-11-02 14:24:52 -0500

    Ball freezes at Z=0 and Y=0
    Stops ball at home plate and keeps ball from clipping through floor.
    
    TODO:
    1) Freeze ball when it hits batter box, not just Z=0
    2) Kill script updates when ball freezes (for performance)
    3) Activate game physics when it hits the floor / misses the batter box for some added fun

--- Assets/BallThrower.cs
+++ Assets/BallThrower.cs
@@ -8,13 +8,17 @@ public class buttonThrow : MonoBehaviour
 {
     public Rigidbody rb;
     private float c = 0.304f;
-    private float t;
-    public Vector3 initBallPosition = new Vector3(-0.76814f, 50f, 6.12393f);
-    public float x, xv, xa; // = 54.30401*c, -132.6675*c, 16.49743*c;
-    public float y, yv, ya; // = 6.27917*c, -4.53417*c, -7.10428*c;
-    public float z, zv, za; // = -0.88788*c, 3.88111*c, -6.30341*c;
+    private float t = 0.0f;
+    public float x0 = 54.30401f;
+    public float xv = -132.6675f;
+    public float xa = 16.49743f;
+    public float y0 = 6.27917f;
+    public float yv = -4.53417f;
+    public float ya = -7.10428f;
+    public float z0 = -0.88788f;
+    public float zv = 3.88111f;
+    public float za = -6.30341f;
 
-    private bool isMoving = true;
     public Button throwButton;
 
 
@@ -28,29 +32,39 @@ public class buttonThrow : MonoBehaviour
         Debug.Log("Throw");
         rb = GetComponent<Rigidbody>();
         // rb.useGravity = true;
-        rb.position = initBallPosition;
-
-        // rb.velocity = new Vector3(v0x, v0y, v0z);
+        rb.position = new Vector3(x0, y0, z0);
         // isMoving = true;
-        rb.GetComponent<TrailRenderer>().Clear();
     }
     void Stop()
     {
         Debug.Log("Stop");
         // rb.useGravity = false;
-        rb.position = initBallPosition;
+        // rb.position = initBallPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
         float tsqr = Mathf.Pow(t, 2);
-        float tz = -x - xv * t - xa * tsqr;
-        float ty = y + yv * t + ya * tsqr;
-        float tx = - z - zv * t - za * tsqr;
+        float z = c*(-x0 - xv * t - xa * tsqr);
+        float y = c*(y0 + yv * t + ya * tsqr);
+        float x = c*(-z0 - zv * t - za * tsqr);
 
-        rb.position = new Vector3(tx, ty, tz);
-        // Debug.Log(rb.transform.position);
+        rb.position = new Vector3(x, y, z);
+        if (t < 0.05f)
+        {
+            ClearTrail();
+        }
+        if (z < 0.0 && y > 0.0)      // if position (z) = home plate, time stops updating
+        {
+            t += Time.deltaTime;
+        }
+
+        Debug.Log($"X={x} | Y={y} | Z={z} | T={t}");
     }
+    void ClearTrail()
+        {
+            rb.GetComponent<TrailRenderer>().Clear();
+            Debug.Log("Trail Cleared");
+        }
 }
